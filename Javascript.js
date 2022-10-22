abbc7b90b5e8956fecaebb074ddbe4c6ae805d1d
piyushmpawar777 <115936644+piyushmpawar777@users.noreply.github.com>
2022-10-22 16:24:24 +0530

    Create Javascript.js

---
+++ Javascript.js
@@ -0,0 +1,38 @@
+(function() {
+  "use strict";
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  });
+})()
